d2e366ac9a39e8d4ab5eaee5483d6b5219c6baa1
82108 <tkdrb1361@naver.com>
2022-06-13 21:50:47 +0900

    회원가입 테스트 뷰 향상

--- src/main/java/com/studyolls/studyolls/account/AccountService.java
+++ src/main/java/com/studyolls/studyolls/account/AccountService.java
@@ -4,9 +4,16 @@ import com.studyolls.studyolls.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +23,12 @@ public class AccountService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void processNewAccount(SignUpForm signUpForm) {
+    public Account processNewAccount(SignUpForm signUpForm) {
         Account newAccount= saveNewAccount(signUpForm);
         newAccount.generateEmailCheckToken();
         sendSignUpCofirmEmail(newAccount);
+
+        return newAccount;
     }
 
     private Account saveNewAccount(SignUpForm signUpForm) {
@@ -45,4 +54,11 @@ public class AccountService {
     }
 
 
+    public void login(Account account) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                account.getNickname(),
+                account.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContextHolder.getContext().setAuthentication(token);
+    }
 }
--- src/test/java/com/studyolls/studyolls/account/AccountControllerTest.java
+++ src/test/java/com/studyolls/studyolls/account/AccountControllerTest.java
@@ -7,8 +7,11 @@ import com.studyolls.studyolls.domain.Account;
 import org.mockito.configuration.*;
 
 import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,7 +27,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AccountControllerTest {
@@ -38,6 +43,41 @@ public class AccountControllerTest {
     @MockBean
     private JavaMailSender javaMailSender;
 
+
+    @DisplayName("인증 메일 확인 -입력값 오류")
+    @Test
+    void checkEmailToken_with_wrong_input()throws Exception{
+        mockMvc.perform(get("/check-email/token")
+                .param("token","sdfksjadkfj")
+                .param("email","uiwv29l@naver.com"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("error"))
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(unauthenticated());
+    }
+
+    @DisplayName("인증 메일 확인 -입력값 정상")
+    @Test
+    void checkEmailToken()throws Exception{
+        Account account = Account.builder()
+                .email("test@email.com")
+                .password("1234567")
+                .nickname("kesd")
+                .build();
+        Account newAccount=accountRepository.save(account);
+        newAccount.generateEmailCheckToken();
+
+        mockMvc.perform(get("/check-email-token")
+                        .param("token",newAccount.getEmailCheckToken())
+                        .param("email","test@email.com"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("nickname"))
+                .andExpect(model().attributeExists("numberOfUser"))
+                .andExpect(view().name("account/checked-email"))
+                .andExpect(authenticated());
+    }
+
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     void signpForm() throws Exception {
@@ -45,7 +85,8 @@ public class AccountControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
-                .andExpect(model().attributeExists("signUpForm"));
+                .andExpect(model().attributeExists("signUpForm"))
+                .andExpect(unauthenticated());
     }
 
     @DisplayName("회원 가입 처리 - 입력값 오류")
@@ -57,7 +98,8 @@ public class AccountControllerTest {
                         .param("password", "12345")
                         .with(csrf()))
                 .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
+                .andExpect(view().name("account/sign-up"))
+                .andExpect(unauthenticated());
     }
     @DisplayName("회원 가입 처리 - 입력값 정상")
     @Test
@@ -68,7 +110,8 @@ public class AccountControllerTest {
                         .param("password", "12345678")
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/"));
+                .andExpect(view().name("redirect:/"))
+                .andExpect(authenticated());
 
         Account account=accountRepository.findByEmail("email@naver.com");
         assertNotNull(account);
