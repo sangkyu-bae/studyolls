1981b5bc52f402e2c37200d37d8a1d1ea947cb25
82108 <tkdrb1361@naver.com>
2022-06-14 22:13:24 +0900

    뷰화면 정리를 위한 모듈 다운등 설정

--- src/main/java/com/studyolls/studyolls/config/SecurityConfig.java
+++ src/main/java/com/studyolls/studyolls/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +27,10 @@ public class SecurityConfig {
 //    private final AccountService accountService;
     private final DataSource dataSource;
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
     /**
      * Spring Security 5.7.x 부터 WebSecurityConfigurerAdapter 는 Deprecated.
      * -> SecurityFilterChain, WebSecurityCustomizer 를 상황에 따라 빈으로 등록해 사용한다.
@@ -44,6 +50,23 @@ public class SecurityConfig {
 //                .rememberMe().userDetailsService(accountService).tokenRepository(tokenRepository())
 //                .and().build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+//        return http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .cors().and()
+//                .headers().frameOptions().disable().and()
+//                .authorizeRequests()
+//                .antMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+//                         "/email-login", "/check-email-login", "login-link", "/profile/*").permitAll()
+//                .antMatchers(HttpMethod.GET, "/profile/*").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .build();
+//    }
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
